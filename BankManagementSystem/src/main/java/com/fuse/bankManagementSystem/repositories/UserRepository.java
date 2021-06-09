d4b8f2abee23ff6f85563783d30785c2e11b77af
Sad-ikshya <kidas201@gmail.com>
2021-06-10 01:28:48 +0545

    change to mongoRepository

--- BankManagementSystem/src/main/java/com/fuse/bankManagementSystem/repositories/UserRepository.java
+++ BankManagementSystem/src/main/java/com/fuse/bankManagementSystem/repositories/UserRepository.java
@@ -2,13 +2,13 @@ package com.fuse.bankManagementSystem.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.fuse.bankManagementSystem.entities.Gender;
 import com.fuse.bankManagementSystem.entities.UserEntity;
 
-public interface UserRepository extends PagingAndSortingRepository<UserEntity, String> {
+public interface UserRepository extends MongoRepository<UserEntity, String> {
 	@Query("{'gender':?0}")
 	public Page<UserEntity> findByGender(Gender gender, Pageable pageRequest);
 }
